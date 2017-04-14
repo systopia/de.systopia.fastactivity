ccce04289eea0f8fd1a4aafd63e02d7257eb88ae
Matthew Wire <mjw@mjwconsult.co.uk>
2017-04-14 11:45:16 +0100

    Selecting a random target contact slows it down way too much on a large dataset

--- CRM/Fastactivity/BAO/Activity.php
+++ CRM/Fastactivity/BAO/Activity.php
@@ -97,13 +97,13 @@ SELECT
   activity.activity_date_time                                                        AS activity_date_time,
   activity.status_id                                                                 AS activity_status_id,
   COUNT(DISTINCT(sources.contact_id))                                                AS source_count,
-  COALESCE(source_contact_me.id, source_contact_random.id)                           AS source_contact_id,   
-  COALESCE(source_contact_me.display_name, source_contact_random.display_name)       AS source_display_name,   
-  COUNT(DISTINCT(assignees.contact_id))                                              AS assignee_count,   
-  COALESCE(assignee_contact_me.id, assignee_contact_random.id)                       AS assignee_contact_id,   
-  COALESCE(assignee_contact_me.display_name, assignee_contact_random.display_name)   AS assignee_display_name, 
-  COALESCE(target_contact_me.id, target_contact_random.id)                           AS target_contact_id,   
-  COALESCE(target_contact_me.display_name, target_contact_random.display_name)       AS target_display_name 
+  COALESCE(source_contact_me.id, source_contact_random.id)                           AS source_contact_id,
+  COALESCE(source_contact_me.display_name, source_contact_random.display_name)       AS source_display_name,
+  COUNT(DISTINCT(assignees.contact_id))                                              AS assignee_count,
+  COALESCE(assignee_contact_me.id, assignee_contact_random.id)                       AS assignee_contact_id,
+  COALESCE(assignee_contact_me.display_name, assignee_contact_random.display_name)   AS assignee_display_name,
+  COALESCE(target_contact_me.id)                                                     AS target_contact_id,
+  COALESCE(target_contact_me.display_name)                                           AS target_display_name 
 FROM civicrm_activity_contact acon 
 LEFT JOIN civicrm_activity activity                ON acon.activity_id = activity.id 
 LEFT JOIN civicrm_activity_contact sources         ON (activity.id = sources.activity_id AND sources.record_type_id = 2) 
@@ -112,8 +112,7 @@ LEFT JOIN civicrm_contact source_contact_me        ON (sources.contact_id = sour
 LEFT JOIN civicrm_activity_contact assignees       ON (activity.id = assignees.activity_id AND assignees.record_type_id = 1) 
 LEFT JOIN civicrm_contact assignee_contact_random  ON (assignees.contact_id = assignee_contact_random.id AND assignee_contact_random.is_deleted = 0) 
 LEFT JOIN civicrm_contact assignee_contact_me      ON (assignees.contact_id = assignee_contact_me.id AND assignee_contact_me.id = %1) 
-LEFT JOIN civicrm_activity_contact targets         ON (activity.id = targets.activity_id AND targets.record_type_id = 3)
-LEFT JOIN civicrm_contact target_contact_random    ON (targets.contact_id = target_contact_random.id AND target_contact_random.is_deleted = 0) 
+LEFT JOIN civicrm_activity_contact targets         ON (activity.id = targets.activity_id AND targets.record_type_id = 3) 
 LEFT JOIN civicrm_contact target_contact_me        ON (targets.contact_id = target_contact_me.id AND target_contact_me.id = %1) 
 {$caseFilter}
 WHERE {$whereClause} 
